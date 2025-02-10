c5f7b7371bf302750bfdddf3cd1c602e08ec12b1
Rhitam Bhuiya <rhitambhuiya@yahoo.com>
2025-02-10 08:51:52 +0530

    Add files via upload

---
+++ Prefix_Evaluation.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stack>
+#include <limits.h>
+#include <algorithm>
+using namespace std;
+int op(char op, int a, int b)
+{
+    switch(op)
+    {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+    }
+}
+
+int evalPrefix(string s)
+{
+    stack<int> st;
+    int res = 0;
+    string temp = "";
+    int n = s.length();
+    char c;
+    for(int i = 0; i < n; i++)
+    {
+        if(s[i] != ' '){
+            c = s[i];
+            temp = c + temp;
+            if(i != n - 1)
+                continue;
+        }
+
+        if(temp == "")
+            continue;
+        
+        if(temp !=  "+" && temp != "-" && temp != "*" && temp != "/")
+            st.push(stoi(temp));
+        else
+        {
+            if(st.size() < 2)
+                return INT_MIN;
+            int op1 = st.top();
+            st.pop();
+            int op2 = st.top();
+            st.pop();
+            res = op(c, op1, op2);
+            st.push(res);
+        }
+        temp = "";
+    }
+
+    if(st.size() > 1)
+        return INT_MIN;
+    return st.top();
+}
+
+int main()
+{
+    cout << "Enter a postfix expression with operators and operands separated by a single space: ";
+    string s;
+    getline(cin, s);
+    reverse(s.begin(), s.end());
+    cout << s;
+    int val = evalPrefix(s);
+    if(val == INT_MIN)
+        cout << endl << "Invalid expression!" << endl;
+    else
+        cout << endl << "Evaluated value: " << val;
+    return 0;
+}
